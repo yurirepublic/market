7c97876a4926ea46da250d0f1ba05eb191e6aa22
ahodewanai <ahodewanai@gmail.com>
2021-02-18 01:28:53 +0800

    完善套利调仓

--- server_src/scripts/套利自动调仓.py
+++ server_src/scripts/套利自动调仓.py
@@ -5,38 +5,30 @@ import time
 import tools
 import json
 import math
+import multiprocessing
 
 
 class Script(tools.Script):
-    def future_to_base(self, amount: str):
+    def info(self):
+        info = tools.ScriptInfo()
+        info.title = "套利自动调仓"
+        info.description = """初代版本，有待改进
+        注意加仓限制和减仓限制，针对的都是期货账户的USDT余额
+        每次调仓会让期货USDT余额尽可能逼近加仓和减仓之间
+        所以加仓限制和减仓限制太过接近，将会频繁调仓导致亏损
+        TODO 添加下单失败重下机制
+        TODO 制定严谨的错误处理链，从可靠的自动处理，到无法自动处理的语音电话警报
         """
-        将期货账户USDT转账到现货账户
-        """
-        self.operator.request('api', '/sapi/v1/asset/transfer', 'POST', {
-            'type': 'MAIN_UMFUTURE',
-            'asset': 'USDT',
-            'amount': amount,
-            'timestamp': binance_api.get_timestamp()
-        })
-
-    def base_to_future(self, amount: str):
-        """
-        将现货账户USDT转账到期货账户
-        """
-        self.operator.request('api', '/sapi/v1/asset/transfer', 'POST', {
-            'type': 'UMFUTURE_MAIN',
-            'asset': 'USDT',
-            'amount': amount,
-            'timestamp': binance_api.get_timestamp()
-        })
+        info.inputs.append(tools.ScriptInput('调仓的交易对', 'close_symbol', ''))
+        info.inputs.append(tools.ScriptInput('减仓限制', 'close_limit', 200))
+        info.inputs.append(tools.ScriptInput('加仓限制', 'open_limit', 1500))
+        return info
 
     def main(self):
-        self.operator = binance_api.Operator()
-        self.operator_future = binance_api.Operatorfuture()
-        operator = self.operator
-        operator_future = self.operator_future
+        operator = binance_api.SmartOperator()     # 实例化一个币安api的操作者
 
-        close_symbol = 'ANKRUSDT'       # 期货账户保证金不足时用于平仓的账户
+        # 从用户输入获取
+        close_symbol = 'ANKRUSDT'       # 期货账户保证金不足时用于平仓的货币对
         close_limit = 200           # 低于此金额，将会减杠杆
         open_limit = 1500           # 高于此金额，将会加杠杆
 
@@ -45,46 +37,79 @@ class Script(tools.Script):
         注：每次调仓会将仓位调到close和open的中间
         """
 
-        # 获取期货可用USDT保证金
         while True:
-            usdt_free = json.loads(operator.request('api', '/api/v3/account', 'GET', {
-                'timestamp': binance_api.get_timestamp()
-            }))['balances']
-            usdt_free = float(
-                list(filter(lambda x: x['asset'] == 'USDT', usdt_free))[0]['free'])
-            usdt_future_free = json.loads(operator_future.request('fapi', '/fapi/v2/balance', 'GET', {
-                'timestamp': binance_api.get_timestamp()
-            }))
-            usdt_future_free = float(
-                list(filter(lambda x: x['asset'] == 'USDT', usdt_future_free))[0]['availableBalance'])
+            # 获取余额
+            usdt_main = operator.get_asset_amount('USDT', 'MAIN')
+            usdt_future = operator.get_asset_amount('USDT', 'FUTURE')
 
             # 判断future账户余额是否低于减仓线
-            if usdt_future_free < close_limit:
-                # 计算需要让合约账户增长多少USDT 两个阈值的中间值减去余额
-                need_usdt = (open_limit - close_limit) / 2 - usdt_free
-                # 判断当前余额是否足够使用
-                if usdt_free > need_usdt:
+            if usdt_future < close_limit:
+                # 计算期货账户缺多少钱  中位数 - 期货余额 = 缺多少钱
+                need = (open_limit - close_limit) / 2 - usdt_future
+                # 判断当前main余额是否足够使用
+                if usdt_main > need:
                     # 足够的话，直接划转需要的钱到期货
-                    self.base_to_future(
-                        binance_api.float_to_str_floor(need_usdt))
+                    operator.transfer_asset('MAIN_UMFUTURE', 'USDT', need)
                 else:
-                    # 不够的话，只要余额不是0，就全划过去
-                    if usdt_free != 0:
-                        self.base_to_future(
-                            binance_api.float_to_str_floor(usdt_free))
+                    # 不够的话，只要main余额不是0，就全划过去
+                    if usdt_main != 0:
+                        operator.transfer_asset(
+                            'MAIN_UMFUTURE', 'USDT', usdt_main)
 
-                    # 如果需要平仓的钱低于15USDT，直接不理（交易最低为10USDT，考虑冗余留到15USDT）
-                    if need_usdt - usdt_free < 15:
-                        pass
-                    else:
-                        # 使用市价单的成交额模式下单
+                    # 计算划转之后还需要平仓多少钱
+                    need_close = need - usdt_main
+                    # 需要平仓的钱必须大于15刀才生效
+                    if need_close > 15:
+                        # 计算需要平仓的货币数
+                        need_close = need_close / \
+                            operator.get_latest_price(close_symbol, 'MAIN')
+                        # 将货币数转为精度对应的str
+                        need_close = binance_api.float_to_str_floor(
+                            need_close, operator.get_symbol_precision(close_symbol))
+                        # 如果转了之后不为0，则启动两边减仓
 
-                        pass
-                # 判断future账户余额是否高于加仓线
+                        def _close_main():
+                            operator.trade_market(
+                                close_symbol, 'MAIN', need_close, 'SELL')
 
-                # TODO 把加仓部分也写上
+                        def _close_future():
+                            operator.trade_market(
+                                close_symbol, 'FUTURE', need_close, 'BUY')
 
-                # if usdt_future_free > open_limit:
-                #     pass
+                        if float(need_close) != 0:
+                            handel1 = multiprocessing.Process(
+                                target=_close_main)
+                            handel2 = multiprocessing.Process(
+                                target=_close_future)
+                            handel1.start()
+                            handel2.start()
+                            handel1.join()
+                            handel2.join()
+
+            # 判断future账户是否高于加仓线
+            if usdt_future > open_limit:
+                # 计算需要加仓多少      持有货币 - 中位数 = 多出来的钱
+                need_open = usdt_future - (open_limit - close_limit) / 2
+                # 把要加仓的钱丢到现货
+                operator.transfer_asset('UMFUTURE_MAIN', 'USDT', need_open)
+                # 把加仓的钱乘以90%换算成货币数（留余，以免钱不够开仓失败）
+                need_open = (need_open * 0.9) / \
+                    operator.get_latest_price(close_symbol, 'MAIN')
+                # 期货现货反向开仓
+
+                def _open_main():
+                    operator.trade_market(
+                        close_symbol, 'MAIN', need_open, 'BUY')
+
+                def _open_future():
+                    operator.trade_market(
+                        close_symbol, 'FUTURE', need_open, 'SELL')
+
+                handel1 = multiprocessing.Process(target=_open_main)
+                handel2 = multiprocessing.Process(target=_open_future)
+                handel1.start()
+                handel2.start()
+                handel1.join()
+                handel2.join()
 
             time.sleep(refresh_timeout)
