921c6c587cd801e9a2df4106f83987bfe76d9ea1
ahodewanai <ahodewanai@gmail.com>
2021-03-01 19:01:25 +0800

    新增全仓余额及划转

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,42 +1,63 @@
 <template>
   <div
-    class="p-2 d-flex flex-column"
-    style="background-color: #fafafa; min-width: 17rem"
+      class="p-2 d-flex flex-column"
+      style="background-color: #fafafa; min-width: 17rem"
   >
     <div class="mb-2 d-flex justify-content-between align-items-center">
       <span class="font-weight-bold">USDT资产</span>
       <div class="d-flex">
         <ClickableIcon
-          class=""
-          name="ri-arrow-left-right-line"
-          @click="show_transfer = show_transfer ? false : true"
+            class=""
+            name="ri-arrow-left-right-line"
+            @click="show_transfer = show_transfer ? false : true"
         />
-        <RefreshButton :anime="refresh_button_anime" @click="refresh" />
+        <RefreshButton :anime="refresh_button_anime" @click="refresh"/>
       </div>
     </div>
 
-    <InfoItem header="可用现货" :content="usdt_free" footer="USDT" />
+    <InfoItem header="可用现货" :content="main_free" footer="USDT"/>
     <div class="d-flex" v-if="show_transfer">
       <TransferInput
-        class="mr-1"
-        placeholder="转到现货"
-        :disabled="disabled_transfer_button"
-        v-model="to_main_value"
-        @click="transfer_to_main"
+          class="mr-1"
+          placeholder="转到现货"
+          :disabled="disabled_transfer_button"
+          v-model="future_to_main_value"
+          @click="future_to_main"
       >
         <b-icon icon="box-arrow-up"></b-icon>
       </TransferInput>
       <TransferInput
-        class="ml-1"
-        placeholder="转到期货"
-        :disabled="disabled_transfer_button"
-        v-model="to_future_value"
-        @click="transfer_to_future"
+          class="ml-1"
+          placeholder="转到期货"
+          :disabled="disabled_transfer_button"
+          v-model="main_to_future_value"
+          @click="main_to_future"
       >
         <b-icon icon="box-arrow-down"></b-icon>
       </TransferInput>
     </div>
-    <InfoItem header="可用期货" :content="usdt_future_free" footer="USDT" />
+    <InfoItem header="可用期货" :content="future_free" footer="USDT"/>
+    <div class="d-flex" v-if="show_transfer">
+      <TransferInput
+          class="mr-1"
+          placeholder="转到现货"
+          :disabled="disabled_transfer_button"
+          v-model="margin_to_main_value"
+          @click="margin_to_main"
+      >
+        <b-icon icon="box-arrow-up"></b-icon>
+      </TransferInput>
+      <TransferInput
+          class="ml-1"
+          placeholder="转到全仓"
+          :disabled="disabled_transfer_button"
+          v-model="main_to_margin_value"
+          @click="main_to_margin"
+      >
+        <b-icon icon="box-arrow-down"></b-icon>
+      </TransferInput>
+    </div>
+    <InfoItem header="可用全仓" :content="margin_free" footer="USDT"/>
   </div>
 </template>
 
@@ -50,14 +71,18 @@ export default {
   name: "Wallet",
   data: function () {
     return {
-      usdt_free: "",
-      usdt_future_free: "",
+      main_free: "",
+      future_free: "",
+      margin_free: '',
       refresh_button_anime: false,
 
       disabled_transfer_button: false,
 
-      to_main_value: "",
-      to_future_value: "",
+      future_to_main_value: "",
+      main_to_future_value: "",
+
+      margin_to_main_value: '',
+      main_to_margin_value: '',
 
       show_transfer: false,
     };
@@ -67,71 +92,117 @@ export default {
   },
   methods: {
     // 转账操作
-    transfer_to_main(event) {
+    future_to_main(event) {
       this.disabled_transfer_button = true;
       this.showToast().info("开始转账");
       this.method_request("transfer", [
         "UMFUTURE_MAIN",
         "USDT",
-        this.to_main_value,
+        this.future_to_main_value,
       ])
-        .then((res) => {
-          this.showToast().success("转账成功");
-          this.to_main_value = "";
-          this.refresh();
-        })
-        .catch((err) => {
-          this.showToast().error("转账失败");
-          this.refresh();
-        })
-        .finally(() => {
-          this.disabled_transfer_button = false;
-        });
+          .then((res) => {
+            this.showToast().success("转账成功");
+            this.future_to_main_value = "";
+            this.refresh();
+          })
+          .catch((err) => {
+            this.showToast().error("转账失败");
+            this.refresh();
+          })
+          .finally(() => {
+            this.disabled_transfer_button = false;
+          });
     },
 
-    transfer_to_future(event) {
+    main_to_future(event) {
       this.disabled_transfer_button = true;
       this.showToast().info("开始转账");
       this.method_request("transfer", [
         "MAIN_UMFUTURE",
         "USDT",
-        this.to_future_value,
+        this.main_to_future_value,
       ])
-        .then((res) => {
-          this.showToast().success("转账成功");
-          this.to_future_value = "";
-          this.refresh();
-        })
-        .catch((err) => {
-          this.showToast().error("转账失败");
-          this.refresh();
-        })
-        .finally(() => {
-          this.disabled_transfer_button = false;
-        });
+          .then((res) => {
+            this.showToast().success("转账成功");
+            this.main_to_future_value = "";
+            this.refresh();
+          })
+          .catch((err) => {
+            this.showToast().error("转账失败");
+            this.refresh();
+          })
+          .finally(() => {
+            this.disabled_transfer_button = false;
+          });
     },
+
+    margin_to_main(event) {
+      this.disabled_transfer_button = true
+      this.showToast().info('开始转账')
+      this.method_request('transfer', [
+        'MARGIN_MAIN',
+        'USDT',
+        this.margin_to_main_value
+      ])
+          .then((res) => {
+            this.showToast().success("转账成功");
+            this.main_to_future_value = "";
+            this.refresh();
+          })
+          .catch((err) => {
+            this.showToast().error("转账失败");
+            this.refresh();
+          })
+          .finally(() => {
+            this.disabled_transfer_button = false;
+          });
+    },
+
+    main_to_margin(event) {
+      this.disabled_transfer_button = true
+      this.showToast().info('开始转账')
+      this.method_request('transfer', [
+        'MAIN_MARGIN',
+        'USDT',
+        this.main_to_margin_value
+      ])
+          .then((res) => {
+            this.showToast().success("转账成功");
+            this.main_to_future_value = "";
+            this.refresh();
+          })
+          .catch((err) => {
+            this.showToast().error("转账失败");
+            this.refresh();
+          })
+          .finally(() => {
+            this.disabled_transfer_button = false;
+          });
+    },
+
     refresh() {
       this.refresh_button_anime = true;
 
       this.method_request("wallet_money", [])
-        .then((res) => {
-          this.usdt_free = res["data"]["usdt_free"];
-          this.usdt_future_free = res["data"]["usdt_future_free"];
+          .then((res) => {
+            this.main_free = res["data"]["main_free"];
+            this.future_free = res["data"]["future_free"];
+            this.margin_free = res['data']['margin_free'];
 
-          this.$toast.open({
-            message: "成功获取钱包金额",
-            type: "success",
+            this.$toast.open({
+              message: "成功获取钱包金额",
+              type: "success",
+            });
+          })
+          .catch((error) => {
+            this.$toast.open({
+              message: "钱包金额获取失败",
+              type: "error",
+            });
+          })
+          .finally(() => {
+            this.refresh_button_anime = false;
           });
-        })
-        .catch((error) => {
-          this.$toast.open({
-            message: "钱包金额获取失败",
-            type: "error",
-          });
-        })
-        .finally(() => {
-          this.refresh_button_anime = false;
-        });
     },
   },
   components: {
