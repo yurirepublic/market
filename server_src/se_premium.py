c42fcbf1f21f94596fc9e059a6d822e99bbf397a
ahodewanai <ahodewanai@gmail.com>
2021-03-25 18:14:03 +0800

    修正脚本错误

--- server_src/se_premium.py
+++ server_src/se_premium.py
@@ -13,26 +13,20 @@ class Script(object):
         self.dc.add_update_callback(callback)
 
     def calc_premium(self, data: data_center.DataWrapper):
-        # 判断是哪边的币价更新了
+        # 提取出币的符号
         if 'main' in data.get_tags():
-            mode = 'main'
             special_tags = data.get_tags() - {'price', 'main'}
         else:
-            mode = 'future'
             special_tags = data.get_tags() - {'price', 'future'}
         # 返回的数据tag数异常直接返回
         if len(special_tags) != 1:
             return
         # 获取交易符号
         symbol = special_tags.pop()
-        # 获取另一方的价格
-        if mode == 'main':
-            main_price = data.get()
-            future_price = self.dc.get({'price', 'future', symbol}).get()
-        else:
-            main_price = self.dc.get({'price', 'main', symbol}).get()
-            future_price = data.get()
-        # 如果有数据为None则表示对方数据缺失，直接返回
+        # 获取双方价格
+        main_price = self.dc.get({'price', 'main', symbol})
+        future_price = self.dc.get({'price', 'future', symbol})
+        # 如果有一方数据缺失直接返回
         if main_price is None or future_price is None:
             return
         # 计算溢价并且放回去
