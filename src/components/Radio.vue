a2e1a5e38d18ebb73b69b8ccb81e95c8edba2a10
ahodewanai <ahodewanai@gmail.com>
2021-04-01 15:04:56 +0800

    新增Radio组件

---
+++ src/components/Radio.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button
+        class="p-1 small"
+        v-for="(option, index) in options"
+        v-bind:class="{'isActive': active === option, 'mr-1': index != options.length - 1}"
+        @click.stop="active = option;$emit('click', option)"
+    >
+      {{ option }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Radio",
+  props: {
+    options: {
+      type: Array,
+      default: []
+    },
+  },
+  data: function () {
+    return {
+      active: ''
+    }
+  },
+  mounted: function () {
+    this.active = this.options[0]
+  }
+
+}
+</script>
+
+<style scoped>
+button {
+  border-radius: 5px;
+  border-style: none;
+  background-color: #e1e1e1;
+  color: #000;
+}
+.isActive {
+  background-color: #505050;
+  color: #fff;
+}
+</style>
