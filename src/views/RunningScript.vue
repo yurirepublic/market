186317445d9306df4db2418850efe2b2019b9b0f
ahodewanai <ahodewanai@gmail.com>
2021-05-07 23:09:21 +0800

    优化脚本管理界面

--- src/views/RunningScript.vue
+++ src/views/RunningScript.vue
@@ -7,6 +7,10 @@
           <span class='font-weight-bold'>运行中的脚本</span>
           <refresh-button :anime='loading' @click='Refresh' />
         </div>
+        <div class='d-flex justify-content-between align-content-center'>
+          <span class='small'>选择服务器</span>
+          <my-radio :options='radioOptions' :active='radioActive' @click='radioActive=$event' />
+        </div>
         <div>
           <table class='table table-hover table-borderless table-sm small'>
             <thead>
@@ -19,7 +23,7 @@
             </thead>
             <tbody>
             <tr
-              v-for='item in running_script_list'
+              v-for='item in runningScriptList'
               :key="item['thread_id']"
               @click='ShowFocusLog(item)'
             >
@@ -47,8 +51,6 @@
             </tbody>
           </table>
         </div>
-
-
       </card-frame>
 
       <card-frame class='ml-1' v-if='loadingThreadId !== null'>
@@ -66,7 +68,7 @@
             <span>正在加载线程 {{ loadingThreadId }} 的Log</span>
           </div>
           <div v-if='!loadingLog'>
-            <pre>{{ focus_log }}</pre>
+            <pre>{{ focusLog }}</pre>
           </div>
         </div>
       </card-frame>
@@ -76,32 +78,66 @@
 </template>
 
 <script>
-import CardFrame from '@/components/CardFrame.vue'
-import RefreshButton from '@/components/RefreshButton.vue'
+import CardFrame from '@/components/CardFrame'
+import RefreshButton from '@/components/RefreshButton'
+import MyRadio from '@/components/MyRadio'
 
 export default {
   name: 'RunningScript',
+  components: {
+    CardFrame,
+    RefreshButton,
+    MyRadio
+  },
   data: function() {
     return {
-      running_script_list: [],
+      runningScriptList: [],
 
-      loading: false,
+      radioOptions: [],
+      radioActive: '',
 
-      loadingLog: false,
-      loadingThreadId: null,
+      apiUrl: {},   // key是nickname， value是可以直接访问的url
 
-      focus_log: '' // 选中脚本的log
+      loading: false, // 正在加载脚本
+
+      loadingLog: false,    // 正在加载log
+      loadingThreadId: null,    // 正在加载线程ID
+
+      focusLog: '', // 选中脚本的log
+      focusUrl: '', // 当前选中服务器的的url，包含ip和端口，可直接访问
+
+      ws: null
     }
   },
-  mounted: function() {
-    this.Refresh()
+  watch: {
+    radioActive: function(newVal) {
+      this.focusUrl = this.apiUrl[newVal]
+      this.Refresh()
+    }
+  },
+  mounted: async function() {
+    this.ws = await this.connectDataCenter()
+
+    // 获取服务器和端口
+    let ip = await this.ws.getDict(['server', 'info', 'ip'])
+    this.radioOptions = Object.keys(ip)
+
+    for (const nickname of Object.keys(ip)) {
+      let port = await this.ws.getDict(['server', 'info', 'port', nickname])
+      // 生成url
+      this.apiUrl[nickname] = 'https://' + ip[nickname] + ':' + port['api']
+    }
+
+    this.radioActive = this.radioOptions[0]
+
+    // await this.Refresh()
   },
   methods: {
     Refresh: async function() {
       this.loading = true
       try {
-        let res = await this.apiRequest('running_script', [])
-        this.running_script_list = res.data
+        let res = await this.apiRequest('running_script', [], this.focusUrl)
+        this.runningScriptList = res.data
         this.showToast.success('运行中的脚本列表获取成功')
       } catch {
         this.showToast.error('运行中的脚本列表获取失败')
@@ -112,8 +148,8 @@ export default {
     RefreshLog: async function() {
       this.loadingLog = true
       try {
-        let res = await this.apiRequest('script_log', [this.loadingThreadId])
-        this.focus_log = res.data
+        let res = await this.apiRequest('script_log', [this.loadingThreadId], this.focusUrl)
+        this.focusLog = res.data
         this.showToast.success('获取选中脚本log成功')
       } catch {
         this.showToast.error('获取选中脚本log失败')
@@ -124,7 +160,7 @@ export default {
     StopScript: async function(item) {
       this.loading = true
       try {
-        await this.apiRequest('stop_script', [item['thread_id']])
+        await this.apiRequest('stop_script', [item['thread_id']], this.focusUrl)
         this.showToast.success('脚本终止成功')
       } catch {
         this.showToast.error('脚本终止失败')
@@ -138,10 +174,6 @@ export default {
       this.loadingThreadId = item['thread_id']
       await this.RefreshLog()
     }
-  },
-  components: {
-    CardFrame,
-    RefreshButton
   }
 }
 </script>
--- src/views/ScriptManage.vue
+++ src/views/ScriptManage.vue
@@ -1,10 +1,14 @@
 <template>
   <div class='p-1 d-flex justify-content-center'>
     <div>
-      <card-frame style='max-height: 100%'>
+      <card-frame style='width: 15rem'>
         <div class='mb-2 d-flex justify-content-between align-items-center'>
           <span class='font-weight-bold'>脚本列表</span>
-          <refresh-button :anime='refresh_button_anime' @click='Refresh' />
+          <refresh-button :anime='isLoading' @click='Refresh' />
+        </div>
+        <div class='d-flex justify-content-between align-content-center'>
+          <span class='small'>选择服务器</span>
+          <my-radio :options='radioOptions' :active='radioActive' @click='radioActive=$event' />
         </div>
         <div style='overflow: auto; max-height: 30rem'>
           <table class='table table-hover table-borderless table-sm small'>
@@ -15,7 +19,7 @@
             </thead>
             <tbody>
             <tr
-              v-for='item in script_list'
+              v-for='item in scriptList'
               :key="item['file_name']"
               @click='ClickScript(item)'
             >
@@ -30,16 +34,16 @@
       </card-frame>
     </div>
     <div class='ml-1'>
-      <card-frame style='max-height: 100%' v-if="focus_item != ''">
+      <card-frame style='max-height: 100%' v-if='focusItem !== null'>
         <div class='mb-2 d-flex justify-content-between align-items-center'>
           <span class='font-weight-bold'>自定义脚本输入</span>
         </div>
         <div>
-          <pre>标题: {{ focus_item['title'] }}</pre>
-          <pre>描述: {{ focus_item['description'] }}</pre>
+          <pre>标题: {{ focusItem['title'] }}</pre>
+          <pre>描述: {{ focusItem['description'] }}</pre>
           <trade-input
             class='mt-1'
-            v-for="item in focus_item['inputs']"
+            v-for="item in focusItem['inputs']"
             :key="item['file_name']"
             :header="item['show_text']"
             :value="item['default']"
@@ -50,9 +54,9 @@
           <button
             class='btn btn-secondary btn-sm mt-2'
             type='button'
-            @click='RunScript(focus_item)'
+            @click='RunScript(focusItem)'
             @click.stop
-            :disabled='button_disabled'
+            :disabled='isRunningRequest'
           >
             开始运行
           </button>
@@ -63,71 +67,97 @@
 </template>
 
 <script>
-import CardFrame from '@/components/CardFrame.vue'
-import RefreshButton from '@/components/RefreshButton.vue'
-import TradeInput from '@/components/TradeInput.vue'
-import InfoItem from '@/components/InfoItem.vue'
+import CardFrame from '@/components/CardFrame'
+import RefreshButton from '@/components/RefreshButton'
+import TradeInput from '@/components/TradeInput'
+import InfoItem from '@/components/InfoItem'
+import MyRadio from '@/components/MyRadio'
 
 export default {
   name: 'ScriptManage',
-  data: function() {
-    return {
-      script_list: [],
-      button_disabled: false,
-      refresh_button_anime: false,
-      focus_item: {}
-    }
-  },
-  mounted: function() {
-    this.Refresh()
-  },
-  methods: {
-    Refresh: function() {
-      this.refresh_button_anime = true
-      this.apiRequest('script_list', [])
-        .then((res) => {
-          this.showToast.success('脚本列表获取成功')
-          this.script_list = res.data
-        })
-        .catch((err) => {
-          this.showToast.error('脚本列表获取失败')
-        })
-        .finally(() => {
-          this.refresh_button_anime = false
-        })
-    },
-    RunScript: function(item) {
-      console.log('将要运行', item)
-      // 将用户的输入提取成字典
-      let input_dict = {}
-      this.focus_item['inputs'].forEach((e) => {
-        input_dict[e['var_name']] = e['default']
-      })
-      // 发送请求
-      this.button_disabled = true
-      this.apiRequest('run_script', [item['file_name'], input_dict])
-        .then((res) => {
-          this.showToast.success('脚本运行成功')
-        })
-        .catch((err) => {
-          this.showToast.error('脚本运行失败')
-        })
-        .finally(() => {
-          this.button_disabled = false
-        })
-    },
-    ClickScript: function(item) {
-      this.focus_item = item
-    },
-    InputScriptOption: function(item) {
-      console.log(item)
-    }
-  },
   components: {
     CardFrame,
     RefreshButton,
     TradeInput,
-    InfoItem
+    InfoItem,
+    MyRadio
+  },
+  data: function() {
+    return {
+      scriptList: [],
+
+      isLoading: false,
+      isRunningRequest: false,
+
+      focusItem: null,
+
+      radioOptions: [],
+      radioActive: '',
+
+      apiUrl: {},   // key是nickname， value是可以直接访问的url
+
+      focusLog: '', // 选中脚本的log
+      focusUrl: '', // 当前选中服务器的的url，包含ip和端口，可直接访问
+
+      ws: null
+    }
+  },
+  watch: {
+    radioActive: function(newVal) {
+      this.focusUrl = this.apiUrl[newVal]
+      this.focusItem = null
+      this.scriptList = []
+      this.Refresh()
+    }
+  },
+  mounted: async function() {
+    this.ws = await this.connectDataCenter()
+
+    let ip = await this.ws.getDict(['server', 'info', 'ip'])
+    this.radioOptions = Object.keys(ip)
+
+    for (const nickname of Object.keys(ip)) {
+      let port = await this.ws.getDict(['server', 'info', 'port', nickname])
+      // 生成url
+      this.apiUrl[nickname] = 'https://' + ip[nickname] + ':' + port['api']
+    }
+
+    this.radioActive = this.radioOptions[0]
+  },
+  methods: {
+    Refresh: async function() {
+      this.isLoading = true
+      try {
+        let res = await this.apiRequest('script_list', [], this.focusUrl)
+        this.showToast.success('脚本列表获取成功')
+        this.scriptList = res.data
+      } catch {
+        this.showToast.error('脚本列表获取失败')
+      } finally {
+        this.isLoading = false
+      }
+    },
+    RunScript: async function(item) {
+      console.log('将要运行', item)
+      // 将用户的输入提取成字典
+      let inputDict = {}
+      for (const e of this.focusItem['inputs']) {
+        inputDict[e['var_name']] = e['default']
+      }
+      // 发送请求
+      this.isRunningRequest = true
+      try {
+        await this.apiRequest('run_script', [item['file_name'], inputDict], this.focusUrl)
+        this.showToast.success('脚本运行成功')
+      } catch {
+        this.showToast.error('脚本运行失败')
+      } finally {
+        this.isRunningRequest = false
+      }
+    },
+    ClickScript: function(item) {
+      this.focusItem = item
+    }
   }
 }
 </script>
